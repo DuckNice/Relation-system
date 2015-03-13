7d2912ba6d18ad692a2d5623577ed950c6671781
Kasper Andkjær <sswkasper@hotmail.com>
2015-03-13 11:49:43 +0100

    some console code.

--- RelationSystem/Program.cs
+++ RelationSystem/Program.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Windows.Forms;
 using System.Collections.Generic;
 using System.Threading;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+using System.IO;
+using System.Text;
+
 
     //Namespaces
 using NRelationSystem;
@@ -10,14 +16,48 @@ namespace RelationSystemProgram
 {
     partial class Program
     {
+
+
+
 		volatile RelationSystem relationSystem = new RelationSystem ();
         
             //Threading work.
         Thread NPCThread;
         private volatile bool stopNPCLoop = false;
 
+        [DllImport("kernel32.dll",
+            EntryPoint = "GetStdHandle",
+            SetLastError = true,
+            CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern IntPtr GetStdHandle(int nStdHandle);
+        [DllImport("kernel32.dll",
+            EntryPoint = "AllocConsole",
+            SetLastError = true,
+            CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern int AllocConsole();
+        private const int STD_OUTPUT_HANDLE = -11;
+        private const int MY_CODE_PAGE = 437;  
+
+
+
+
 		public Program()
         {
+            AllocConsole();
+            IntPtr stdHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+            SafeFileHandle safeFileHandle = new SafeFileHandle(stdHandle, true);
+            FileStream fileStream = new FileStream(safeFileHandle, FileAccess.Write);
+            Encoding encoding = System.Text.Encoding.GetEncoding(MY_CODE_PAGE);
+            StreamWriter standardOutput = new StreamWriter(fileStream, encoding);
+            standardOutput.AutoFlush = true;
+            Console.SetOut(standardOutput);  
+
+
+
+
+
 			SetupActions ();
 			CreateFirstMasks();
 			CreateFirstPeople();
