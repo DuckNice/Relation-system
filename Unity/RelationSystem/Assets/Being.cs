42b65f994433d51990f87b9a7b5a45a98c515fc7
Kasper Andkjær <sswkasper@hotmail.com>
2015-04-01 15:43:02 +0200

    ¤%& macs -.-

--- Unity/RelationSystem/Assets/Being.cs
+++ Unity/RelationSystem/Assets/Being.cs
@@ -43,82 +43,86 @@ public class Being
 	}
 
 
-	public void NPCAction()
+	public void NPCAction(float time)
 	{
-        Person self = maskSystem.pplAndMasks.GetPerson(name);
-
-		
-		if (currentRule != null && actionStartTime + currentRule.actionToTrigger.duration > Time.time) 
-		{
-            currentRule.SustainAction(self, currentRule.selfOther[self], currentRule, misc: possessions.ToArray());
-		}
-		else
-		{
-            List<PosActionItem> possibleActions = new List<PosActionItem>();
-			//debug.Write("BEFORE LOOP "+maskSystem.historyBook.Count);
+        if (name.ToLower() != "player")
+        {
+            Person self = maskSystem.pplAndMasks.GetPerson(name);
 
 
-            for (int i = maskSystem.historyBook.Count - 1; i >= 0; i--)
+            if (currentRule != null && actionStartTime + currentRule.actionToTrigger.duration > time)
             {
-                HistoryItem item = maskSystem.historyBook[i];
-				//debug.Write("START LOOP "+item.GetRule ().ruleName+" "+item.GetRule().rulesThatMightHappen.Count);
-
-                if (item.GetTime() < Time.time - reactMemory)
-                {
-					debug.Write("BREAKS");
-                    break;
-                }
-				 
-                if (item.HasReacted(self) || item.GetDirect() != self)
-                {
-					debug.Write("CONTINUES");
-					continue;
-                }
-
-				debug.Write("INSIDE LOOP "+item.GetRule ().ruleName+" "+item.GetRule().rulesThatMightHappen.Count);
-
-                foreach (Rule rule in item.GetRule().rulesThatMightHappen)
-                {
-                    int index = possibleActions.FindIndex(x => x.action == rule.actionToTrigger);
-
-                    Person subject = item.GetSubject();
-
-                    if (index < 0)
-                    {
-                        possibleActions.Add(new PosActionItem(rule.actionToTrigger, subject));
-                    }
-                    else if (!possibleActions[index].reactToPerson.Contains(subject))
-                    {
-                        possibleActions[index].reactToPerson.Add(subject);
-                    }
-                }
+                currentRule.SustainAction(self, currentRule.selfOther[self], currentRule, misc: possessions.ToArray());
             }
-			if (debug.Toggle) {
-				debug.Write ("---------- " + self.name + "'s TURN.");
-			}
-
-            debug.Write("action length = " + possibleActions.Count + ".");
-
-			Rule _rule = self.GetAction (notPossibleActions, possibleActions, focus.Values.ToList ());
-
-            debug.Write("rule: " + _rule.ruleName + " chosen.");
-
-			if (debug.Toggle)
-			{
-				debug.Write ("Doing action '" + _rule.actionToTrigger.name + "' from " + name + ".");
-			}
-
-			if (_rule.actionToTrigger.name.ToLower () != "empty") 
-			{
-                currentRule = _rule;
-                actionStartTime = Time.time;
-
-				_rule.DoAction (self, _rule.selfOther [self], _rule, misc: possessions.ToArray ());
-			}
             else
             {
-                currentRule = null;
+                List<PosActionItem> possibleActions = new List<PosActionItem>();
+                //debug.Write("BEFORE LOOP "+maskSystem.historyBook.Count);
+
+
+                for (int i = maskSystem.historyBook.Count - 1; i >= 0; i--)
+                {
+                    HistoryItem item = maskSystem.historyBook[i];
+                    //debug.Write("START LOOP "+item.GetRule ().ruleName+" "+item.GetRule().rulesThatMightHappen.Count);
+
+                    if (item.GetTime() < time - reactMemory)
+                    {
+                        debug.Write("BREAKS");
+                        break;
+                    }
+
+                    if (item.HasReacted(self) || item.GetDirect() != self)
+                    {
+                        debug.Write("CONTINUES");
+                        continue;
+                    }
+
+                    debug.Write("INSIDE LOOP " + item.GetRule().ruleName + " " + item.GetRule().rulesThatMightHappen.Count);
+
+                    foreach (Rule rule in item.GetRule().rulesThatMightHappen)
+                    {
+                        int index = possibleActions.FindIndex(x => x.action == rule.actionToTrigger);
+
+                        Person subject = item.GetSubject();
+
+                        if (index < 0)
+                        {
+                            possibleActions.Add(new PosActionItem(rule.actionToTrigger, subject));
+                        }
+                        else if (!possibleActions[index].reactToPerson.Contains(subject))
+                        {
+                            possibleActions[index].reactToPerson.Add(subject);
+                        }
+                    }
+                }
+                if (debug.Toggle)
+                {
+                    debug.Write("---------- " + self.name + "'s TURN.");
+                }
+
+                debug.Write("action length = " + possibleActions.Count + ".");
+
+                Rule _rule = self.GetAction(notPossibleActions, possibleActions, focus.Values.ToList());
+
+                debug.Write("rule: " + _rule.ruleName + " chosen.");
+
+                if (debug.Toggle)
+                {
+                    debug.Write("Doing action '" + _rule.actionToTrigger.name + "' from " + name + ".");
+                }
+
+                if (_rule.actionToTrigger.name.ToLower() != "empty")
+                {
+                    currentRule = _rule;
+                    actionStartTime = Time.time;
+
+                    _rule.DoAction(self, _rule.selfOther[self], _rule, misc: possessions.ToArray());
+                }
+                else
+                {
+                    currentRule = null;
+                }
             }
-		}
+        }
     }
 }
