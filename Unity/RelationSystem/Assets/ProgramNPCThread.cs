f8db27a6820153371324f58b72f3db4161be4b00
Bjarke Larsen <bjarke@larsen-dk.dk>
2015-04-06 18:49:34 +0200

    this apparently didn't commit

--- Unity/RelationSystem/Assets/ProgramNPCThread.cs
+++ Unity/RelationSystem/Assets/ProgramNPCThread.cs
@@ -55,4 +55,57 @@ public partial class Program : MonoBehaviour
 	{
 		shouldPlay = playToggle.isOn;
 	}
+
+
+
+	
+	
+	void UpdateStats(){
+		statsString = "";
+		
+		foreach (Person p in relationSystem.pplAndMasks.people.Values) {
+			statsString += p.name+"\n";
+			statsString += "AngFea: "+p.moods[MoodTypes.angryFear]+"\n";
+			statsString += "aroDis: "+p.moods[MoodTypes.arousDisgus]+"\n";
+			statsString += "EnrTir: "+p.moods[MoodTypes.energTired]+"\n";
+			statsString += "HapSad: "+p.moods[MoodTypes.hapSad]+"\n";
+			statsString += "\n";
+		}
+		statsString += "Money: \n";
+		
+		
+		foreach (Being b in beings) {
+			statsString +=  b.name+"  "+ b.possessions.Find(x=>x.Name == "money").value+"\n";
+		}
+
+		statsString += "\n\n\nMask Links: \n";
+
+
+		foreach (Person p in relationSystem.pplAndMasks.people.Values) {
+			statsString += p.name+"\n";
+			statsString += ""+p.selfPerception.roleName+" "+p.selfPerception.GetlvlOfInfl()+"\n";
+
+			foreach(Link l in p.culture){
+				statsString += l.roleName+" "+l.GetlvlOfInfl();
+				if(l.roleRef.Count == 0){ statsString += "\n"; }
+				foreach(Person rr in l.roleRef){
+					statsString += " "+rr.name+"\n";
+				}
+			}
+			foreach(Link l in p.interPersonal){
+				statsString += l.roleName+" "+l.GetlvlOfInfl();
+				if(l.roleRef.Count == 0){ statsString += "\n"; }
+				foreach(Person rr in l.roleRef){
+					statsString += " "+rr.name+"\n";
+				}
+			}
+			statsString += "\n";
+		}
+
+
+	}
+
+
+
+
 }
